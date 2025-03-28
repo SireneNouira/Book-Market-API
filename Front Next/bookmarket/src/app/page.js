bfa88458ecf98fc47061ac7da2a4da44ee3018a1
SireneNouira <sirene.nouira16@gmail.com>
2025-03-28 12:03:18 +0100

    test tailwind

--- Front Next/bookmarket/src/app/page.js
+++ Front Next/bookmarket/src/app/page.js
@@ -7,9 +7,9 @@ export default function HomePage() {
   return (
     <>
       <Header />
-      <FeaturedBooks />
+      {/* <FeaturedBooks />
       <Filter />
-      <BookList />
+      <BookList /> */}
     </>
   );
 }
---
+++ Front Next/bookmarket/src/components/common/Header.js
@@ -0,0 +1,11 @@
+function Header() {
+  return (
+    <div className="flex justify-center bg-emerald-400 text-white  shadow-lg">
+      <p className="text-sm text-emerald-700 flex items-center">
+        Achats et Ventes de Livres d'Occasions
+      </p>
+    </div>
+  );
+}
+
+export default Header;
