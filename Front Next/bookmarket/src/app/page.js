4ffa0c6a7dabae6fa9d01e82e1715c1726aaa25a
SireneNouira <sirene.nouira16@gmail.com>
2025-03-28 11:17:34 +0100

    add: structure de base front

--- Front Next/bookmarket/src/app/page.js
+++ Front Next/bookmarket/src/app/page.js
@@ -1,40 +1,15 @@
-"use client";
+// /app/page.js
+import BookList from '../components/home/BookList';
+import FeaturedBooks from '../components/home/FeaturedBooks';
+import Header from '../components/common/Header';
 
-import { useEffect, useState } from "react";
-import api from "@/utils/api";
-
-export default function Home() {
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    api.get("/books") // Remplace "/books" par ton endpoint API
-      .then((response) => {
-        // Accéder à la clé "member" dans la réponse
-        const books = response.data.member || []; // Si "member" existe, c'est un tableau de livres
-        setData(books);
-      })
-      .catch((error) => {
-        console.error("Erreur lors de la récupération des données :", error);
-      });
-  }, []);
-  
+export default function HomePage() {
   return (
-    <div>
-      <h1>Liste des livres</h1>
-      <ul>
-        {Array.isArray(data) ? (
-          data.map((book) => (
-            <li key={book.id}>
-              <h3>{book.title}</h3>
-              <p>{book.description}</p>
-              {/* Affiche d'autres propriétés comme 'prix', 'auteur', etc. */}
-            </li>
-          ))
-        ) : (
-          <p>Aucune donnée disponible.</p>
-        )}
-      </ul>
-    </div>
+    <>
+      <Header />
+      <FeaturedBooks />
+      <Filter />
+      <BookList />
+    </>
   );
-  
 }
